1b9ae55f6aad6cc3e861f086dbfd2d601e679a9d
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2020-07-30 10:29:36 +0300

    Modify SimpleCacheSchemaProvider for use Yii2 caching.

--- src/Schema/Provider/SimpleCacheSchemaProvider.php
+++ src/Schema/Provider/SimpleCacheSchemaProvider.php
@@ -4,18 +4,32 @@ declare(strict_types=1);
 
 namespace Vjik\Yii2\Cycle\Schema\Provider;
 
-use Psr\SimpleCache\CacheInterface;
 use Vjik\Yii2\Cycle\Schema\SchemaProviderInterface;
+use yii\base\BaseObject;
+use yii\caching\CacheInterface;
+use yii\di\Instance;
 
-final class SimpleCacheSchemaProvider implements SchemaProviderInterface
+final class SimpleCacheSchemaProvider extends BaseObject implements SchemaProviderInterface
 {
     public const DEFAULT_KEY = 'Cycle-ORM-Schema';
-    private $cache;
+
+    /**
+     * @var CacheInterface|array|string the cache used to improve RBAC performance. This can be one of the following:
+     *
+     * - an application component ID (e.g. `cache`)
+     * - a configuration array
+     * - a [[\yii\caching\Cache]] object
+     */
+    public $cache = 'cache';
+
     private $key = self::DEFAULT_KEY;
 
-    public function __construct(CacheInterface $cache)
+    public function init()
     {
-        $this->cache = $cache;
+        parent::init();
+        if ($this->cache !== null) {
+            $this->cache = Instance::ensure($this->cache, CacheInterface::class);
+        }
     }
 
     public function withConfig(array $config): SchemaProviderInterface
@@ -27,7 +41,8 @@ final class SimpleCacheSchemaProvider implements SchemaProviderInterface
 
     public function read(): ?array
     {
-        return $this->cache->get($this->key);
+        $value = $this->cache->get($this->key);
+        return is_array($value) ? $value : null;
     }
 
     public function write($schema): bool
